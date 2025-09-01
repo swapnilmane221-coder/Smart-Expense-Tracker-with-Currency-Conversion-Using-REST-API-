e9615543e249e612a7e7dc96f86b7b729ad81c1d
swapnilmane221-coder <swapnilmane2521@gmail.com>
2025-09-01 12:05:28 +0530

    css embedded successfullly

--- Smart_Expense_Tracker_with_Currency_Conversion_Using_REST_API/settings.py
+++ Smart_Expense_Tracker_with_Currency_Conversion_Using_REST_API/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'expenses',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'Smart_Expense_Tracker_with_Currency_Conversion_Using_REST_API.ur
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'users'/'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,9 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS = [BASE_DIR / "static"]  # Optional, for global static files
